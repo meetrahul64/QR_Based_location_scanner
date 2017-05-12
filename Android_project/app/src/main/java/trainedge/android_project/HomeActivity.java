16481fa869d5283c671589872a86dec6b70053bf
meetrahul64 <meetrahul64@gmail.com>
2017-05-12 18:38:43 +0530

    change in nav_header.xml file

--- Android_project/app/src/main/java/trainedge/android_project/HomeActivity.java
+++ Android_project/app/src/main/java/trainedge/android_project/HomeActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.android.gms.appinvite.AppInviteInvitation;
 import com.google.firebase.auth.FirebaseAuth;
@@ -61,6 +62,13 @@ public class HomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        View view = navigationView.getHeaderView(0);
+        TextView tvName = (TextView) view.findViewById(R.id.tvEmail);
+        try{
+            tvName.setText(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+        }catch (Exception e){
+
+        }
     }
 
     @Override
